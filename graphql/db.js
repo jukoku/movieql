8e45ba292e5ff08572d47420f3965e63455a0a5c
Jino Chun <jukoku@gmail.com>
2020-12-31 16:30:38 +0900

    Wrapping a REST API with Graph QL

--- graphql/db.js
+++ graphql/db.js
@@ -1,44 +1,16 @@
-export let movies = [
-  {
-    id: 1,
-    name: "Avengers - The new one",
-    score: 8,
-  },
-  {
-    id: 2,
-    name: "Steel Rain 2: Summit",
-    score: 5,
-  },
-  {
-    id: 3,
-    name: "Proximity",
-    score: 2,
-  },
-];
+import fetch from "node-fetch";
 
-export const getMovies = () => movies;
+const API_URL = "https://yts.mx/api/v2/list_movies.json?";
 
-export const getById = (id) => {
-  const filteredMovie = movies.filter((movie) => movie.id === id);
-  return filteredMovie[0];
-};
-
-export const deleteMovie = (id) => {
-  const cleanedMovies = movies.filter((movie) => movie.id !== id);
-  if (movies.length > cleanedMovies.length) {
-    movies = cleanedMovies;
-    return true;
-  } else {
-    return false;
+export const getMovies = (limit, rating) => {
+  let REQUEST_URL = API_URL;
+  if (limit > 0) {
+    REQUEST_URL += `limit=${limit}`;
   }
-};
-
-export const addMovie = (name, score) => {
-  const newMovie = {
-    id: `${movies.length + 1}`,
-    name,
-    score,
-  };
-  movies.push(newMovie);
-  return newMovie;
+  if (rating > 0) {
+    REQUEST_URL += `&minimum_rating=${rating}`;
+  }
+  return fetch(REQUEST_URL)
+    .then((res) => res.json())
+    .then((json) => json.data.movies);
 };
